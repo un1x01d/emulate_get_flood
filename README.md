bc7455591616194d084873ae52fd6a69ff1434d7
ZeD <zed@ZeDs-MacBook-Pro.local>
2022-01-22 21:37:14 -0500

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # emulate_get_flood
 This tool emulates a HTTP GET attack with random IPs in the header and random User-Agents
+
+## DO NOT USE THIS TOOL FOR ANY MALICIOUS ACTIVITY!!! - This method of "attack" is very poor and outdated, and is useful ONLY for testing.
+
+## Usage
+```
+./emulate_get_flood.sh -x [threads] -t [timeout] -u [url]
+```
---
+++ emulate_get_flood.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+usage() {
+  echo "$0 -x [threads] -t [time]"
+}
+
+IFS=$'\n'
+url="http://localhost"
+agents_list="full.txt"
+
+while getopts "x:t:u:" arg; do
+  case $arg in
+    x)
+       workers="${OPTARG}"
+      ;;
+    t)
+       time="${OPTARG}"
+      ;;
+    u)
+       url="${OPTARG}"
+      ;;
+    *)
+       usage ; exit 1 
+      ;;
+  esac
+done
+
+if [ -z $url ] || [ -z $time ]; then
+   echo "i need a parameter" ; exit 1 
+fi
+
+run_curl() {
+     for job in `sort -R $agents_list | head -$(wc -l $agents_list| awk '{print $1}')` ;do
+	     curl --silent --output /dev/null --header \
+          "X-Forwarded-for: $((RANDOM % 256)).$((RANDOM % 256)).$((RANDOM % 256)).$((RANDOM % 256))" -A $job $url 
+     done
+}
+
+tput setaf 6 ; echo "[+] Starting load test with $1 workers for $time seconds. `tput setaf 5; date`"
+for i in $workers ; do
+   for worker in `seq $workers`; do
+      run_curl $i & PID="$!"
+      tput setaf 3 ; echo "[+] Starting worker $(tput setaf 2)[ $(tput setaf 7 ; echo -n "$worker" ; tput setaf 2) ] - pid: $(tput setaf 7 ; echo -n "$PID" ; tput setaf 2)"
+      sleep 0.05
+      tput sgr0 
+   done
+done
+
+sleep $time && echo "Terminating all workers" && killall bash $0
